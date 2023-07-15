346d51169fba3036bc8ccd3d0e57214627d32fcd
eguire <105729902+eguire@users.noreply.github.com>
2023-07-15 16:50:22 +0300

    Editing functional

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ window.addEventListener('DOMContentLoaded', () => {
 				goalsArray = [];
 
 	// Creating goal
-		
+
 	function createGoal(value, checked = '') {
-		let goal = document.createElement('li');
+		const goal = document.createElement('li');
 		goal.classList.add('list__item');
 
 		goal.innerHTML = `<label class="list__label">
@@ -20,31 +20,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		goalsParent.append(goal);
 
-		goalsArray.push({text: value, checked: checked});
+		goalsArray.push({ text: value, checked: checked });
 
 		setLocalStrg();
 	};
 
 	goalsButton.addEventListener('click', () => {
 		createGoal(goalsInput.value);
+		goalsInput.value = '';
 	});
 
-	// Deleting goal
+	// Deleting and editing goals
 
 	goalsParent.addEventListener('click', (e) => {
 
-		const target = e.target;
+		const target = e.target,
+					userText = document.querySelector('.list__textarea'),
+					spans = document.querySelectorAll('.list__text');
 
-		if (target.classList.contains('list__delete-btn')) {
-			goalsArray.forEach((item, i) => {
-				if (item.text == target.parentElement.innerText) {
-					goalsArray.splice(i, 1);
-				};
-			});
-
-			target.parentElement.remove();
-		};
-
+		// Check and edit checkboxes
 		if (target.classList.contains('list__checkbox')) {
 			goalsArray.forEach((item, i) => {
 				if (item.text == target.nextElementSibling.innerText && target.checked === true) {
@@ -55,7 +49,65 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		};
 
-		setLocalStrg();
+		// Cancel editing on click out of target
+		if (userText && !target.classList.contains('list__textarea') && !target.classList.contains('list__apply-btn') && !target.classList.contains('list__cancel-btn')) {
+			userText.remove();
+
+			spans.forEach(span => {
+				span.style.display = 'block';
+			});
+
+			document.querySelector('.list__apply-btn').classList.remove('list__apply-btn');
+			document.querySelector('.list__cancel-btn').classList.remove('list__cancel-btn');
+		};
+
+		// Editing
+		if (target.classList.contains('list__apply-btn')) { // Apply changes
+			goalsArray.forEach((item, i) => {
+				if (item.text == target.previousElementSibling.children[1].innerText) {
+					goalsArray[i].text = userText.value;
+				}
+			});
+
+			userText.remove();
+			target.previousElementSibling.children[1].innerText = userText.value;
+			target.previousElementSibling.children[1].style.display = 'block';
+
+			target.classList.remove('list__apply-btn');
+			target.nextElementSibling.classList.remove('list__cancel-btn');
+		} else if (target.classList.contains('list__set-btn')) { // Edit goal
+			goalsArray.forEach(item => {
+				if (item.text == target.parentElement.innerText) {
+					const textarea = document.createElement('textarea');
+
+					textarea.classList.add('list__textarea');
+					textarea.innerText = item.text;
+
+					target.previousElementSibling.append(textarea);
+					target.previousElementSibling.children[1].style.display = 'none';
+
+					target.classList.add('list__apply-btn');
+					target.nextElementSibling.classList.add('list__cancel-btn');
+				}
+			});
+		} else if (target.classList.contains('list__cancel-btn')) { // Cancel changes
+
+			userText.remove();
+
+			target.classList.remove('list__cancel-btn');
+			target.previousElementSibling.classList.remove('list__apply-btn');
+			target.parentElement.firstChild.children[1].style.display = 'block';
+		} else if (target.classList.contains('list__delete-btn')) { // Delete goal
+			goalsArray.forEach((item, i) => {
+				if (item.text == target.parentElement.innerText) {
+					goalsArray.splice(i, 1);
+				};
+			});
+
+			target.parentElement.remove();
+		};
+
+		setLocalStrg(); // Save all changes in local storage
 	});
 
 	// Setting in LocalStorage
@@ -63,14 +115,15 @@ window.addEventListener('DOMContentLoaded', () => {
 	function setLocalStrg() {
 		window.localStorage.setItem('goals', JSON.stringify(goalsArray));
 	};
-	
+
 	// Get goals from LS
-	
+
 	function getGoals() {
-	goals = JSON.parse(window.localStorage.getItem('goals'));
-	goals.forEach(el => {
-		createGoal(el.text, el.checked);
-	});
+		goals = JSON.parse(window.localStorage.getItem('goals'));
+
+		goals.forEach(el => {
+			createGoal(el.text, el.checked);
+		});
 	};
 
 	getGoals();
